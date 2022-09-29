fc2dc28fe77df348c8506bd4c47e85be93c387da
Julia Mullen <julia.l.mullen@gmail.com>
2022-09-29 15:53:15 -0400

    directory change

--- parser.py
+++ parser.py
@@ -7,7 +7,7 @@ from biothings import config
 logger = config.logger
 
 def load_annotations(data_folder):
-    with open('/home/jmullen/nde/nde.jsonl') as data:
+    with open('/opt/home/outbreak/outbreak.api/plugins/nde/nde.jsonl') as data:
         for line in data:
             datum = json.loads(line)
             yield datum
